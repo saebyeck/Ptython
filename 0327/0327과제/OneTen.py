56bd6cad75c220eee5f392a198296ce43afb5f4f
saebyeck <alswns070720@gmail.com>
2026-04-03 02:24:04 +0900

    Add files via upload

---
+++ 0327/0327과제/OneTen.py
@@ -0,0 +1,7 @@
+i = 1
+sum = 0
+# i 값이 10보다 작으면 반복
+while i <= 10 :
+    sum = sum + i
+    i = i + 1
+print("합계는", sum)
---
+++ 0327/0327과제/gugu.py
@@ -0,0 +1,6 @@
+dan = int(input("원하는 단은: "))
+i = 1
+
+while i <= 9 :
+    print(f"{dan}*{i}={dan * i}")
+    i = i + 1
